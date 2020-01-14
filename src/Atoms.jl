ad3c712672a43616c273d07b80176b5b0261c007
Yaozhenghang Ma <Yaozhenghang.Ma@gmail.com>
2020-01-14 21:35:25 +0800

    property energy

--- src/Atoms.jl
+++ src/Atoms.jl
@@ -1,6 +1,6 @@
 abstract type AbstractProperties end
 
-mutable struct Properties{T}<: AbstractProperties where T<:Any 
+mutable struct Properties{T<:Any}<: AbstractProperties 
     name::String 
     value::T
 
@@ -10,22 +10,39 @@ mutable struct Properties{T}<: AbstractProperties where T<:Any
     end
 end
 
-function (prop::Properties)()
+mutable struct Energy <: AbstractProperties
+    name::String
+    value::Float64
+
+    Energy() = new("Energy", 0.0)
+    Energy(val::Float64) = new("Energy", val)
+    Energy(val::Real) = Energy(Float64(val))
+end
+
+function Base.eltype(prop::AbstractProperties)
+    return typeof(prop.value)
+end
+
+function (prop::AbstractProperties)()
     return prop.value 
 end
 
-function (prop::Properties{T})(val::T) where T<:Any 
-    prop.value = val 
+function (prop::AbstractProperties)(val)
+    if typeof(val) != eltype(prop)
+        throw(ArgumentError("Wrong type for functor."))
+    else
+        prop.value = val 
+    end
 end
 
-function Base.show(io::IO, prop::Properties)
+function Base.show(io::IO, prop::AbstractProperties)
     print(prop.name, "=", prop.value)
 end
 
 mutable struct Atoms
-    atom::Array{Atom, 1}
+    atom::Array{AbstractAtom, 1}
     number::Int64
-    Properties::Array{Properties, 1}
+    Properties::Array{AbstractProperties, 1}
 
     Atoms() = new(Array{Atom, 1}(undef, 0), 0, Array{Properties, 1}(undef, 0))
     function Atoms(atom::Array{Atom, 1}, num::Int64, prop::Array{Properties, 1})
