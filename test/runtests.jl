5394e0462ebf76d69d37546140ef24652853f585
Yaozhenghang Ma <Yaozhenghang.Ma@gmail.com>
2020-01-15 14:57:22 +0800

    Add test for Atom.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using AtomBase
 using Test
 
-@testset "AtomBase.jl" begin
-    # Write your own tests here.
+@testset "Atom.jl" begin
+    atom1 = Atom()
+    atom2 = Atom("H", [1.0, 0.0, 0.0])
+    io = IOBuffer()
+    show(io, atom2)
+    @test atom2() == ("H", [1.0, 0.0, 0.0])
+    @test !(atom1 == atom2)
+    @test String(take!(io)) == "H\t[1.0, 0.0, 0.0]"
 end
